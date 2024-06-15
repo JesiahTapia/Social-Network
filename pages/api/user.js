03319676054df668e6fc5fe39e08f4b56f0fbf77
JesiahTapia <siahtapia@gmail.com>
2024-06-15 09:50:42 -0400

    Functioning

---
+++ pages/api/user.js
@@ -0,0 +1,29 @@
+// // Creates account
+
+// import { url } from "inspector"
+
+// @param {string} userID
+// @param {string} email
+// @param {string} password
+// @param {string} name
+
+// const promise = account.create{ID.unique(), email, password, name};
+
+// const account = new Account(appwriteClient);
+
+// @param {string} email
+// @param {string} password
+
+// const promise = account.createEmailSession{email, password};
+
+// const account = new Account(appwriteClient);
+// const currentAccount = account.get()
+
+// @param {string} sessionID
+
+// const promise = account.deleteSession('current')
+
+// @param {string} email
+// @param {string} url
+
+// const promise = account.createRecovery(email, url)
